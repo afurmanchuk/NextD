cc730634aedd7baae66b7ad8f7b7ac50f3bb39e2
afurmanchuk <furmanchuk@icnanotox.org>
2019-11-26 13:54:18 -0600

    2019-11-26

--- NextD_Demographic_GPCsites-2018-12-05-SX.sql
+++ NextD_Demographic_GPCsites-2018-12-05-SX.sql
@@ -1,17 +1,18 @@
 /******************************************************************************************************************/
-/* NextD Clinical Variable Extractions                                                                            */
-/* - require: 1. FinalStatTable1: the local version where dates are unshifted                                     */
-/*            2. date_unshifts: an intermediate table for recovering real dates                                   */
-/*               columns:        PATID: CDM patient ID                                                            */
-/*                               days_shift: days shifted                                                         */
-/*                               first_enc_date: the real dates of first encounter                                */                                                 */
-/*                               MARITAL_STATUS_CD: marital status (only available in local i2b2)                 */
-/*               - if such intermediate table alread saved locally for table1 extraction, then just use that one; */
-/*                 if not, the NextD_Date_Recovery.sql will do the work                                           */
-/* - We assume PCORNET_CDM is set appropriate for your site; for example, define PCORNET_CDM = PCORNET_CDM_C5R2   */
+/* NextD Clinical Variable Extractions - Demographic Table                                                       */
 /******************************************************************************************************************/
 
-/*Note: 'KUMC-specific' issue are marked as such*/
+/* Tables required in this code: 
+- 1. FinalStatsTable1_local: the local version of FinalStatsTable1 (output of SQLTable1_GPCsites_orcale-2019-11-25-SX.sql)                             
+- 2. &&PCORNET_CDM.DEMOGRAPHIC                       
+- 3. &&I2B2_ID.patient_dimension               
+*/
+
+/*global parameters:
+ &&PCORNET_CDM: name of CDM schema (>v5.0)
+ &&I2B2_ID: name of local identified i2b2 schema
+ "KUMC specific" fields: may need to be adjusted with local EMR values
+*/
 
 /*MARITAL_STATUS categories (KUMC-specific):
  --S - Single
@@ -23,29 +24,26 @@
  --NI - Unknown
 */
 
-/**************************************************************************/
-/***********************Table 2 -- Demographics****************************/
-/**************************************************************************/
---modified: re-numbered 'Pipe' and included in the output table
 create table NEXTD_DEMOGRAPHIC as
 select pat.PATID,'|' as Pipe1
-      ,cast(to_char(demo.BIRTH_DATE+ds.days_shift,'YYYY') as INTEGER) BIRTH_YEAR,'|' as Pipe2
-      ,cast(to_char(demo.BIRTH_DATE+ds.days_shift ,'MM') as INTEGER) BIRTH_MONTH,'|' as Pipe3
-      ,demo.BIRTH_DATE+ds.days_shift-pat.FirstVisit as BIRTH_DELTA_DAYS,'|' as Pipe4
+      ,cast(to_char(demo.BIRTH_DATE,'YYYY') as INTEGER) BIRTH_YEAR,'|' as Pipe2
+      ,cast(to_char(demo.BIRTH_DATE,'MM') as INTEGER) BIRTH_MONTH,'|' as Pipe3
+      ,demo.BIRTH_DATE-pat.FirstVisit as BIRTH_DELTA_DAYS,'|' as Pipe4
       ,demo.SEX,'|' as Pipe5
       ,demo.RACE,'|' as Pipe6
       ,demo.HISPANIC,'|' as Pipe7
       ,demo.PAT_PREF_LANGUAGE_SPOKEN,'|' as Pipe8
-      ,case when ds.MARITAL_STATUS_CD in ('u','@') or ds.MARITAL_STATUS_CD is null then 'NI'
-            else upper(ds.MARITAL_STATUS_CD) end as MARITAL_STATUS
+      ,case when i2b2.MARITAL_STATUS_CD in ('u','@') or i2b2.MARITAL_STATUS_CD is null then 'NI'
+            else upper(i2b2.MARITAL_STATUS_CD) end as MARITAL_STATUS   -- KUMC specific
       ,'ENDALONAEND' as ENDOFLINE
-from date_unshifts ds 
-join FinalStatTable1 pat
-on ds.PATID = pat.PATID
-left join /*provide current PCORNET_CDM.Demogrphic table here*/ "&&PCORNET_CDM".DEMOGRAPHIC demo
-on ds.PATID = demo.PATID
-left join date_unshifts ds
-on ds.PATIENT_NUM_I2B2 = ds.PATIENT_NUM
+from FinalStatsTable1_local pat
+left join "&&PCORNET_CDM".DEMOGRAPHIC demo
+on pat.PATID = demo.PATID
+left join "&&I2B2_ID".patient_dimension i2b2
+on i2b2.patient_num = pat.PATID
 ; 
 
-/*save local NEXTD_DEMOGRAPHIC.csv file*/
+---------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------
+----- 1. Download table NEXTD_DEMOGRAPHIC as .csv  file for final delivery                               ------
+---------------------------------------------------------------------------------------------------------------
