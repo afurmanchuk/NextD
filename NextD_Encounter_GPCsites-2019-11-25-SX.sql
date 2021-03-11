09cc38721b995823e43115300381ff6c01845d53
afurmanchuk <furmanchuk@icnanotox.org>
2021-03-11 09:24:11 -0600

    Update NextD_Encounter_GPCsites-2019-11-25-SX.sql

--- NextD_Encounter_GPCsites-2019-11-25-SX.sql
+++ NextD_Encounter_GPCsites-2019-11-25-SX.sql
@@ -1,64 +1,66 @@
-/****************************************************************************************************************/
-/* NextD Clinical Variable Extractions - Encounter Table                                                        */
-/* no pregnancy exclusion                                                                                       */
-/****************************************************************************************************************/
-
-/* Tables required in this code: 
-- 1. FinalStatsTable1_local: the local version of FinalStatsTable1 (output of SQLTable1_GPCsites_orcale-2019-11-25-SX.sql)                             
-- 2. &&PCORNET_CDM.ENCOUNTER                                   
+---------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------
+-----                             Part 3: Encounters for the study sample                                 -----  
+--------------------------------------------------------------------------------------------------------------- 
+/* Tables for this eaxtraction: 
+1. Table 1 (named here #FinalStatTable1) with Next-D study sample IDs. See separate SQL code for producing this table.
+2. External to PCORNET table ([NextD].[dbo].[NEXT_OriginalNPIFROMBaseTaxonomy]) with NPI values on encounters of interest. 
+Warning: current remapping utilizing toxonomy codes. Sites expected to map NPIs into taxonomy codes by using:
+The full NPI online registry refreshed weekly could be found at https://npiregistry.cms.hhs.gov/
+or complete download of this registry could be found at https://www.cms.gov/Regulations-and-Guidance/Administrative-Simplification/NationalProvIdentStand/DataDissemination.html
+or use file https://www.dropbox.com/home/diabetes%20project%20(working%20docs)/Definitions_StudySamples%26Variables?preview=NPI2ToxonomycodeCorssWalk_2018-01-01_AF.zip (contains two columns: NPI and Taxonomy code)
+due to extreme size of registry I suggest to (i) collect complete list of NPI codes for the site; (ii) map those into Taxonomy codes and save them localy ; (iii) use this subset for all extraction codes utilizing NPI information.
+The table [NextD].[dbo].[NEXT_OriginalNPIFROMBaseTaxonomy] has two columns: [NPI] and [Healthcare Provider Taxonomy Code_1] 
 */
-
-/*global parameters:
- &&PCORNET_CDM: name of CDM schema (>v5.0)
- "KUMC specific" fields: may need to be adjusted with local EMR values
-*/
-
-create table NEXTD_ENCOUNTER as
-with enc_with_age_at_visit as (
-select enc.PATID
-      ,enc.ENCOUNTERID
-      ,enc.PROVIDERID
-      ,enc.ADMIT_DATE
-      ,enc.ADMIT_DATE - fst.FirstVisit as ADMIT_Days_from_FirstEnc
-	  ,enc.ADMITTING_SOURCE 
-	  ,enc.DISCHARGE_DATE
-      ,enc.DISCHARGE_DATE - fst.FirstVisit as DISCHARGE_Days_from_FirstEnc
-      ,enc.DISCHARGE_DISPOSITION
-	  ,enc.DISCHARGE_STATUS 
-      ,enc.ENC_TYPE
-      ,enc.FACILITYID
-	  ,enc.FACILITY_TYPE 
-      ,round((enc.ADMIT_DATE - fst.BIRTH_DATE)/365.25,2) AS age_at_visit
-from "&&PCORNET_CDM".ENCOUNTER enc
-join FinalStatsTable1_local fst on fst.PATID = enc.PATID
-)
-select distinct
-       PATID,'|' as Pipe1
-      ,ENCOUNTERID,'|' as Pipe2
-      ,PROVIDERID,'|' as Pipe3
-      ,cast(to_char(ADMIT_DATE,'YYYY') as INTEGER) ADMIT_YEAR,'|' as Pipe4
-      ,cast(to_char(ADMIT_DATE,'MM') as INTEGER) ADMIT_MONTH,'|' as Pipe5
-      ,ADMIT_Days_from_FirstEnc,'|' as Pipe6
-      ,cast(to_char(DISCHARGE_DATE,'YYYY') as INTEGER) DISCHARGE_YEAR,'|' as Pipe7
-      ,cast(to_char(DISCHARGE_DATE,'MM') as INTEGER) DISCHARGE_MONTH,'|' as Pipe8
-	  ,DISCHARGE_Days_from_FirstEnc,'|' as Pipe9
-      ,ENC_TYPE,'|' as Pipe10
-      ,FACILITYID,'|' as Pipe11
-      ,DISCHARGE_DISPOSITION,'|' as Pipe12
-      ,DISCHARGE_STATUS,'|' as Pipe13
-	  ,ADMITTING_SOURCE,'|' as Pipe14
-      ,FACILITY_TYPE
-      ,'ENDALONAEND' as ENDOFLINE
-from enc_with_age_at_visit
-where age_at_visit between 18 and 89
-;
-
-
-/*for better efficiency*/
-create index NextD_ENC_PAT_IDX on NEXTD_ENCOUNTER(ENCOUNTERID);
-
+--------------------------------------------------------------------------------------------------------------- 
+use /*provide here the name of PCORI database here: */PCORI_SAS;
+--------------------------------------------------------------------------------------------------------------- 
+----  Declare study time frame variables:
+DECLARE @studyTimeRestriction int;declare @UpperTimeFrame int; declare @LowerTimeFrame int;
+-----                             Specify time frame and age limits                                       -----
+--Set extraction time frame below. If time frames not set, the code will use the whole time frame available from the database
+set @LowerTimeFrame=18263;--'2010-01-01';
+set @UpperTimeFrame=22280;--'2020-12-31';
+--set age restrictions:
+declare @UpperAge int; declare @LowerAge int;set @UpperAge=89; set @LowerAge=18;
 ---------------------------------------------------------------------------------------------------------------
----------------------------------------------------------------------------------------------------------------
------ 1. Download table NEXTD_ENCOUNTER as .csv  file for final delivery                                 ------
----------------------------------------------------------------------------------------------------------------
+/* Steps for insurance remap (omit if not remapping):
 
+1. Load provided by NU team remapping table named MasterReMap. It has following columns:
+[RAW_financial_class_dsc],[RAW_cdf_meaning],[RAW_BENEFIT_PLAN_NAME],[RAW_PRODUCT_TYPE],[RAW_PAYOR_NAME],[RAW_EPM_ALT_IDFR],[RAW_SHORT_NAME],[NewCategory]
+2. Load table (named here #RawNPIValuesTable) with coresponding NPI values for each encounter of interest.
+3. Remap raw values into new insurance and provider categories :*/
+---------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------- 
+--------------------------------------------------------------------------------------------------------------- 
+--------------------------------------------------------------------------------------------------------------- 
+select c.PATID,
+		a.ENCOUNTERID,
+		a.PROVIDERID,
+		year(dateadd(dd,a.ADMIT_DATE,'1960-01-01')) as ADMIT_DATE_YEAR,
+		month(dateadd(dd,a.ADMIT_DATE,'1960-01-01')) as ADMIT_DATE_MONTH,
+		a.ADMIT_DATE - c.[FirstVisit] as DAYS_from_FirstEncounter_Date1,
+		year(dateadd(dd,a.DISCHARGE_DATE,'1960-01-01')) as DISCHARGE_DATE_YEAR,
+		month(dateadd(dd,a.DISCHARGE_DATE,'1960-01-01')) as DISCHARGE_DATE_MONTH,
+		a.ADMIT_DATE - c.[FirstVisit] as DAYS_from_FirstEncounter_Date2,
+		a.ENC_TYPE,
+		a.FACILITYID,
+		a.FACILITY_TYPE,
+		a.DISCHARGE_DISPOSITION,
+		a.DISCHARGE_STATUS,
+		a.ADMITTING_SOURCE,
+		a.PROVIDERID,
+		a.[PAYER_TYPE_PRIMARY],
+		a.[PAYER_TYPE_ SECONDARY]
+into #NextD_ENCOUNTER_FINAL
+from /* provide name of table 1 here: */ #FinalTable1 c 
+join [dbo].[ENCOUNTER] a on c.PATID=a.PATID
+join dbo.DEMOGRAPHIC d on c.PATID=d.PATID		
+where convert(numeric(18,6),(a.ADMIT_DATE-d.BIRTH_DATE))/365.25 between @LowerAge and @UpperAge 
+and a.ADMIT_DATE between @LowerTimeFrame and @UpperTimeFrame; 
+--------------------------------------------------------------------------------------------------------------- 
+--------------------------------------------------------------------------------------------------------------- 
+ /* Save #NextD_ENCOUNTER_FINAL as csv file. 
+Use "|" symbol as field terminator and 
+"ENDALONAEND" as row terminator. */ 
+--------------------------------------------------------------------------------------------------------------- 
