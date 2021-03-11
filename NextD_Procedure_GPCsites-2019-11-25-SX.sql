a0e14947a0200c87eec55ec4e7c0f0565c7a3527
afurmanchuk <furmanchuk@icnanotox.org>
2021-03-11 09:25:37 -0600

    Update NextD_Procedure_GPCsites-2019-11-25-SX.sql

--- NextD_Procedure_GPCsites-2019-11-25-SX.sql
+++ NextD_Procedure_GPCsites-2019-11-25-SX.sql
@@ -1,42 +1,47 @@
-/******************************************************************************************************************/
-/* NextD Clinical Variable Extractions - Procedure Table                                                          */
-/* no pregnancy exclusion                                                                                         */
-/******************************************************************************************************************/
-
-/* Tables required in this code: 
-- 1. FinalStatsTable1_local: the local version of FinalStatsTable1 (output of SQLTable1_GPCsites_orcale-2019-11-25-SX.sql)                             
-- 2. NEXTD_ENCOUNTER: output of NextD_Encounter_GPCsites-2019-11-25-SX.sql                       
-- 3. &&PCORNET_CDM.PROCEDURES              
-*/
-
-/*global parameters:
- &&PCORNET_CDM: name of CDM schema (>v5.0)
- "KUMC specific" fields: may need to be adjusted with local EMR values
-*/
-
-create table NEXTD_PROCEDURES as
-select enc.PATID,'|' as Pipe1
-      ,enc.ENCOUNTERID,'|' as Pipe2
-      ,enc.ENC_TYPE,'|' as Pipe3
-      ,enc.ADMIT_YEAR,'|' as Pipe4
-      ,enc.ADMIT_MONTH,'|' as Pipe5
-      ,enc.ADMIT_Days_from_FirstEnc,'|' as Pipe6
-      ,px.PROVIDERID,'|' as Pipe7
-      ,px.PROCEDURESID,'|' as Pipe8
-	  ,cast(to_char(px.PX_DATE,'YYYY') as INTEGER) as PX_YEAR,'|' as Pipe9
-	  ,cast(to_char(px.PX_DATE,'MM') as INTEGER) as  PX_MONTH,'|' as Pipe10
-	  ,round(px.PX_DATE - fst.FirstVisit) as PX_Days_from_FirstEnc,'|' as Pipe11
-      ,px.PX,'|' as Pipe12
-      ,px.PX_TYPE,'|' as Pipe13
-      ,px.PX_SOURCE,'|' as Pipe14
-	  ,px.PPX
-      ,'ENDALONAEND' as ENDOFLINE
-from NEXTD_ENCOUNTER enc
-join "&&PCORNET_CDM".PROCEDURES px on enc.ENCOUNTERID = px.ENCOUNTERID       
-join FinalStatsTable1_local fst on enc.PATID = fst.PATID  
-; 
-
 ---------------------------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------------------
------ 1. Download table NEXTD_PROCEDURES as .csv file for final delivery                                 ------
+-----                             Part 7: Procedures for the study sample                                 -----  
+--------------------------------------------------------------------------------------------------------------- 
+---------------------------------------------------------------------------------------------------------------
+/* Tables for this eaxtraction: 
+1. Table 1 (named here #FinalTable1) with Next-D study sample IDs. See separate SQL code for producing this table.
+2. PROCEDURES and DEMOGRAPHIC table from PCORNET. */
+---------------------------------------------------------------------------------------------------------------
+use /*provide here the name of PCORI database here: */PCORI_SAS;
+---------------------------------------------------------------------------------------------------------------
+-----                            Declare study time frame variables:                                      -----
+DECLARE @studyTimeRestriction int;declare @UpperTimeFrame int; declare @LowerTimeFrame int;
+-----                             Specify time frame and age limits                                       -----
+--Set extraction time frame below. If time frames not set, the code will use the whole time frame available from the database
+set @LowerTimeFrame=18263;--'2010-01-01';
+set @UpperTimeFrame=22280;--'2020-12-31';
+--set age restrictions:
+declare @UpperAge int; declare @LowerAge int;set @UpperAge=89; set @LowerAge=18;
+---------------------------------------------------------------------------------------------------------------
+select c.PATID,
+	b.ENCOUNTERID,
+	b.PROCEDURESID,
+	b.ENC_TYPE,
+	year(dateadd(dd,b.ADMIT_DATE,'1960-01-01')) as ADMIT_DATE_YEAR,
+	month(dateadd(dd,b.ADMIT_DATE,'1960-01-01')) as ADMIT_DATE_MONTH,
+	b.ADMIT_DATE - c.FirstVisit as DAYS_from_FirstEncounter_Date1,
+	b.PROVIDERID,
+	year(dateadd(dd,b.PX_DATE,'1960-01-01')) as PX_DATE_YEAR,
+	month(dateadd(dd,b.PX_DATE,'1960-01-01')) as PX_DATE_MONTH,
+	b.PX_DATE - c.FirstVisit as DAYS_from_FirstEncounter_Date2,
+	b.PX,
+	b.PPX,
+	b.PX_TYPE ,
+	b.PX_SOURCE
+	into #NextD_PROCEDURES_FINAL
+from /* provide name of table 1 here: */ #FinalTable1 c 
+join /* provide name of PCORNET table PROCEDURES here: */ [dbo].[PROCEDURES] b on c.PATID=b.PATID 
+join /* provide name of PCORNET table DEMOGRAPPHIC here: */ [dbo].DEMOGRAPHIC d on c.PATID=d.PATID
+where convert(numeric(18,6),(b.ADMIT_DATE - d.BIRTH_DATE))/365.25 between @LowerAge and @UpperAge 
+	and b.ADMIT_DATE between @LowerTimeFrame and @UpperTimeFrame;
+---------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------
+/* Save #NextD_PROCEDURES_FINAL as csv file. 
+Use "|" symbol as field terminator and 
+"ENDALONAEND" as row terminator. */ 
 ---------------------------------------------------------------------------------------------------------------
