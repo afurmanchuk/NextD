ef0708fab87fde7ec71aee0a181b4e2e204afedb
afurmanchuk <furmanchuk@icnanotox.org>
2021-03-11 09:24:45 -0600

    Update NextD_Labs_GPCsites-2019-11-25-SX.sql

--- NextD_Labs_GPCsites-2019-11-25-SX.sql
+++ NextD_Labs_GPCsites-2019-11-25-SX.sql
@@ -1,178 +1,86 @@
-/******************************************************************************************************************/
-/* NextD Clinical Variable Extractions - Lab Table                                                                */
-/* exclude pregancy                                                                                               */
-/******************************************************************************************************************/
-
-/* Tables required in this code: 
-- 1. FinalStatsTable1_local: the local version of FinalStatsTable1 (output of SQLTable1_GPCsites_orcale-2019-11-25-SX.sql)
-- 2. NextD_distinct_preg_events: an intermediate table with all pregnancy events
-- 3. &&PCORNET_CDM.LAB_RESULT_CM                                     
-*/
-
-/*global parameters:
- &&PCORNET_CDM: name of CDM schema (>v5.0)
- "KUMC specific" fields: may need to be adjusted with local EMR values
-*/
-
-
-create table nextd_labs_loinc as
-select column_value lab_loinc, 'CHOLESTEROL' lab_name
-from table(sys.ODCIVarchar2List('2093-3','14647-2'))
-union all
-select column_value lab_loinc, 'HDL' lab_name
-from table(sys.ODCIVarchar2List('14646-4','18263-4','2085-9'))
-union all
-select column_value lab_loinc, 'TRIGLYCERIDE' lab_name
-from table(sys.ODCIVarchar2List('12951-0','14927-8','2571-8','47210-0'))
-union all
-select column_value lab_loinc, 'LDL' lab_name
-from table(sys.ODCIVarchar2List('12773-8','13457-7','18261-8','18262-6','2089-1','22748-8','39469-2','49132-4','55440-2'))
-union all
-select column_value lab_loinc, 'A1C' lab_name
-from table(sys.ODCIVarchar2List('17855-8', '4548-4','4549-2','17856-6','41995-2','59261-8','62388-4','71875-9','54039-3'))
-union all
-select column_value lab_loinc, 'CREATININE' lab_name
-from table(sys.ODCIVarchar2List('21232-4','38483-4','2160-0','44784-7','40248-7')) 
-union all
-select column_value lab_loinc, 'HGB' lab_name
-from table(sys.ODCIVarchar2List('718-7','20509-6','30313-1','30350-3','30313-1','14775-1','30352-9',
-                                '75928-2','20509-6','55782-7','59260-0'))
-union all
-select column_value lab_loinc, 'RANDOM GLUCOSE' lab_name
-from table(sys.ODCIVarchar2List('2345-7', '2339-0','10450-5','17865-7','1554-5','6777-7','54246-4','2344-0','41652-9'))
-union all
-select column_value lab_loinc, 'FASTING GLUCOSE' lab_name
-from table(sys.ODCIVarchar2List('1558-6', '10450-5','1554-5','17865-7','35184-1'))
-union all
-select column_value lab_loinc, 'MICROALBUMIN' lab_name
-from table(sys.ODCIVarchar2List('14957-5','57369-1','53530-2','30003-8','43605-5','53531-0','11218-5','43607-1',
-                                '63474-1','53532-8','14956-7','43606-3','56553-1','49023-5','58448-2','44292-1',
-                                '14958-3','14959-1','59159-4','30000-4','30001-2','47558-2','13705-9','14585-4',
-                                '1753-3','1754-1','1755-8','1757-4','20621-9','21059-1','9318-7','50949-7','32294-1'))
-union all
-select column_value lab_loinc, 'C-PEPTIDE' lab_name
-from table(sys.ODCIVarchar2List('13032-8','13033-6','13034-4','13035-1','13036-9','13037-7','13038-5','13039-3',
-                                '13040-1','13041-9','13042-7','13043-5','13044-3','13045-0','13859-4','13860-2',
-                                '13861-0','14633-2','16501-9','16502-7','1986-9','25568-7','25569-5','25570-3',
-                                '25571-1','25572-9','25573-7','25574-5','25575-2','25576-0','25577-8','25578-6',
-                                '25579-4','25580-2','25581-0','25582-8','27408-4','27421-7','27839-0','35195-7',
-                                '38249-9','38421-4','38422-2','38423-0','38424-8','38425-5','38426-3','42180-0',
-                                '47583-0','47584-8','47585-5','47586-3','47587-1','47588-9','47589-7','47590-5',
-                                '47591-3','47592-1','47593-9','47594-7','47595-4','47832-1','47833-9','47834-7',
-                                '50461-3','50462-1','50463-9','50464-7','50465-4','50466-2','50467-0','50468-8',
-                                '55918-7','55919-5','56516-8','56582-0','56583-8','56584-6','57376-6','57645-4',
-                                '57646-2','57647-0','57648-8','57649-6','57650-4','57651-2','57894-8','58494-6',
-                                '58495-3','58496-1','58497-9','58498-7','58499-5','58500-0','58501-8','58502-6',
-                                '58503-4','58504-2','58505-9','58506-7','58507-5','58508-3','58509-1','58510-9',
-                                '58511-7','58512-5','58513-3','58514-1','58515-8','58516-6','58517-4','58518-2',
-                                '58519-0','58520-8','58521-6','58522-4','58686-7','58816-0','58841-8','58896-2',
-                                '77610-4','77611-2','77612-0','77651-8','77652-6'))
-union all
-select column_value lab_loinc, 'BLOOD KETONE' lab_name
-from table(sys.ODCIVarchar2List('53061-8'))
-union all
-select column_value lab_loinc, 'URINE KETONE' lab_name
-from table(sys.ODCIVarchar2List('49779-2','33043-1','33903-6','2514-8','50557-8','5797-6','2514-8','33903-6','57734-6'))
-union all
-select column_value lab_loinc, 'AUTOANTIBODIES' lab_name
-from table(sys.ODCIVarchar2List('45225-0','45171-6','5265-4','63571-4','56687-7','8086-1','31547-3','34652-8',
-                                '13927-9','33563-8','31209-0','56718-0','81155-4','32636-3','70253-0','70252-2', /*Islet Cell Antibody*/
-                                '42501-7','13926-1','56540-8','58451-6','81725-4','72523-4','30347-9','83004-2','82660-2', /*Glutamic Acid Decarboxylase (GAD65) Antibody*/
-                                '31209-0','56718-0','81155-4','32636-3','70253-0','70252-2', /*Insulinoma-Associated-2 Autoantibodies (IA-2A)*/
-                                '76651-9' /*Zinc Transporter 8 (ZnT8) Antibody*/))
-;
-
-
-create table NEXTD_LABS as
-with lab_with_age_realdate as (
-select l.PATID
-      ,l.ENCOUNTERID
-      ,l.LAB_RESULT_CM_ID
-      ,l.LAB_ORDER_DATE
-      ,l.SPECIMEN_DATE 
-      ,l.SPECIMEN_SOURCE 
-      ,l.RESULT_QUAL
-      ,l.RESULT_NUM
-      ,l.RESULT_UNIT
-      ,l.LAB_PX
-      ,l.LAB_PX_TYPE
-      ,l.RAW_LAB_NAME
-      ,l.RAW_RESULT
-      ,l.RESULT_MODIFIER
-      ,l.NORM_RANGE_LOW
-      ,l.NORM_MODIFIER_LOW
-      ,l.NORM_RANGE_HIGH 
-      ,l.NORM_MODIFIER_HIGH
-      ,l.LAB_LOINC
-      ,l.RESULT_LOC
-      ,lc.LAB_NAME
-      ,round((l.LAB_ORDER_DATE - fst.BIRTH_DATE)/365.25,2) as age_at_event
-from "&&PCORNET_CDM".LAB_RESULT_CM l
-join FinalStatsTable1_local fst on fst.PATID = l.PATID
-join nextd_labs_loinc lc on lc.LAB_LOINC=l.LAB_LOINC
-)
-   ,lab_pregn_exclud as (
-select lrd.PATID
-      ,lrd.ENCOUNTERID
-      ,lrd.LAB_ORDER_DATE
-      ,lrd.SPECIMEN_DATE 
-      ,lrd.SPECIMEN_SOURCE 
-      ,lrd.RESULT_QUAL
-      ,lrd.RESULT_NUM
-      ,lrd.RESULT_UNIT
-      ,lrd.LAB_PX
-      ,lrd.LAB_PX_TYPE
-      ,lrd.RAW_LAB_NAME
-      ,lrd.RAW_RESULT
-      ,lrd.RESULT_MODIFIER
-      ,lrd.NORM_RANGE_LOW
-      ,lrd.NORM_MODIFIER_LOW
-      ,lrd.NORM_RANGE_HIGH 
-      ,lrd.NORM_MODIFIER_HIGH
-      ,lrd.LAB_LOINC
-      ,lrd.RESULT_LOC
-      ,lrd.LAB_NAME
-from lab_with_age_realdate lrd
-where lrd.age_at_event between 18 and 89 and                             /*age restriction*/
-      lrd.LAB_ORDER_DATE between Date '2010-01-01' and CURRENT_DATE and  /*time restriction*/
-      not exists (select 1 from NextD_distinct_preg_events pd            /*pregenancy exclusion*/                 
-                  where pd.PATID = lrd.PATID and
-                        (abs(lrd.LAB_ORDER_DATE - pd.ADMIT_DATE) <= 365)) 
-)
---time blinding
-select fst.PATID,'|' as Pipe1
-      ,el.ENCOUNTERID,'|' as Pipe2
-      ,cast(to_char(el.LAB_ORDER_DATE,'YYYY') as INTEGER) LAB_ORDER_YEAR,'|' as Pipe3
-      ,cast(to_char(el.LAB_ORDER_DATE,'MM') as INTEGER) LAB_ORDER_MONTH,'|' as Pipe4
-      ,el.LAB_ORDER_DATE - fst.FirstVisit as LAB_ORDER_Days_from_FirstEnc,'|' as Pipe5
-      ,cast(to_char(el.SPECIMEN_DATE ,'YYYY') as INTEGER) SPECIMEN_YEAR,'|' as Pipe6
-      ,cast(to_char(el.SPECIMEN_DATE ,'MM') as INTEGER) SPECIMEN_MONTH,'|' as Pipe7
-      ,round(el.SPECIMEN_DATE  - fst.FirstVisit) as SPECIMEN_Days_from_FirstEnc,'|' as Pipe8
-      ,el.SPECIMEN_SOURCE,'|' as Pipe9
-      ,el.RESULT_QUAL,'|' as Pipe10
-      ,el.RESULT_NUM,'|' as Pipe11
-      ,el.RESULT_UNIT,'|' as Pipe12
-      ,el.LAB_PX,'|' as Pipe13
-      ,el.LAB_PX_TYPE,'|' as Pipe14
-      ,el.RAW_LAB_NAME,'|' as Pipe15
-      ,el.RAW_RESULT,'|' as Pipe16
-      ,el.RESULT_MODIFIER,'|' as Pipe17
-      ,el.NORM_RANGE_LOW,'|' as Pipe18
-      ,el.NORM_MODIFIER_LOW,'|' as Pipe19
-      ,el.NORM_RANGE_HIGH,'|' as Pipe20
-      ,el.NORM_MODIFIER_HIGH,'|' as Pipe21
-      ,el.LAB_LOINC,'|' as Pipe22
-      ,el.RESULT_LOC,'|' as Pipe23
-      ,el.LAB_NAME
-      ,'ENDALONAEND' as ENDOFLINE
-from FinalStatsTable1_local fst
-join lab_pregn_exclud el
-on el.PATID = fst.PATID          
-; 
-
-
 ---------------------------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------------------
------ 1. Download table NEXTD_LAB as .csv  file for final delivery                                     ------
+-----                             Part 8: Labs for the study sample                                       -----  
+--------------------------------------------------------------------------------------------------------------- 
+--------------------------------------------------------------------------------------------------------------- 
+/* Tables for this eaxtraction: 
+1. Table 1 (named here #FinalTable1) with Next-D study sample IDs. See separate SQL code for producing this table.
+2. LAB_RESULT_CM and DEMOGRAPHIC table from PCORNET.
+3. External to PCORNET table CAP_LABS with the labs of interest.*/
 ---------------------------------------------------------------------------------------------------------------
-
+use /*provide here the name of PCORI database here: */PCORI_SAS;
+---------------------------------------------------------------------------------------------------------------
+-----                            Declare study time frame variables:                                      -----
+DECLARE @studyTimeRestriction int;declare @UpperTimeFrame int; declare @LowerTimeFrame int;
+-----                             Specify time frame and age limits                                       -----
+--Set extraction time frame below. If time frames not set, the code will use the whole time frame available from the database
+set @LowerTimeFrame=18263;--'2010-01-01';
+set @UpperTimeFrame=22280;--'2020-12-31';
+--set age restrictions:
+declare @UpperAge int; declare @LowerAge int;set @UpperAge=89; set @LowerAge=18;
+---------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------
+--Part2: Labs from CAPRICORN:
+select c.PATID,
+		b.LAB_RESULT_CM_ID,
+		b.ENCOUNTERID,
+		year(dateadd(dd,b.LAB_ORDER_DATE,'1960-01-01')) as LAB_ORDER_DATE_YEAR,
+		month(dateadd(dd,b.LAB_ORDER_DATE,'1960-01-01')) as LAB_ORDER_DATE_MONTH,
+		datediff(d,c.FirstVisit,b.LAB_ORDER_DATE) as DAYS_from_FirstEncounter_Date1,
+		year(dateadd(dd,b.SPECIMEN_ORDER_DATE,'1960-01-01')) as SPECIMEN_ORDER_DATE_YEAR,
+		month(dateadd(dd,b.SPECIMEN_ORDER_DATE,'1960-01-01')) as SPECIMEN_ORDER_DATE_MONTH,
+		b.SPECIMEN_ORDER_DATE - c.FirstVisit as DAYS_from_FirstEncounter_Date2,
+		b.VALUE_NUMERIC as RESULT_NUM,
+		b.RESULT_UNIT,
+		b.RESULT_QUAL,
+		b.LAB_LOINC,
+		b.LAB_PX,
+		b.LAB_PX_TYPE,
+		b.RESULT_LOC,
+		b.RESULT_MODIFIER,
+		b.NORM_RANGE_LOW,	
+		b.NORM_MODIFIER_LOW, 
+		b.NORM_RANGE_HIGH, 
+		b.NORM_MODIFIER_HIGH, 
+		b.RAW_LAB_NAME
+into #NextD_LABS_FINAL
+from /* provide name of table 1 here: */ #FinalTable1 c 
+join [dbo].[LAB_RESULT_CM] b on c.PATID=b.CAP_ID 
+join dbo.DEMOGRAPHIC d on c.PATID=d.PATID
+where b.VALUE_NUMERIC is not NULL 
+		and (b.LAB_LOINC in ('14647-2','2093-3',
+							'14646-4','18263-4','2085-9',
+							'12951-0','14927-8','2571-8','47210-0',
+							'12773-8','13457-7','18261-8','18262-6','2089-1','22748-8','39469-2','49132-4','55440-2',
+							'17855-8','4548-4','4549-2','17856-6','41995-2','59261-8','62388-4','71875-9','54039-3',
+                             '21232-4','38483-4','2160-0','44784-7','40248-7',
+							 '718-7','20509-6','30313-1','30350-3','30313-1','14775-1','30352-9','75928-2','20509-6','55782-7','59260-0',
+							 '2345-7','2339-0','10450-5','17865-7','1554-5','6777-7','54246-4','2344-0','41652-9',
+							 '1558-6','10450-5', '1554-5', '17865-7', '35184-1',
+							 '14957-5','57369-1','53530-2','30003-8','43605-5','53531-0','11218-5','43607-1','63474-1','53532-8','14956-7','43606-3','56553-1','49023-5','58448-2','44292-1','14958-3',
+													'14959-1','59159-4','30000-4','30001-2','47558-2','13705-9','14585-4','1753-3','1754-1','1755-8','1757-4','20621-9','21059-1','9318-7','50949-7','32294-1',
+							  '13032-8','13033-6','13034-4','13035-1','13036-9','13037-7','13038-5',
+													 '13039-3','13040-1','13041-9','13042-7','13043-5','13044-3','13045-0','13859-4','13860-2','13861-0','14633-2','16501-9','16502-7','1986-9','25568-7','25569-5','25570-3','25571-1',
+													 '25572-9','25573-7','25574-5','25575-2','25576-0','25577-8','25578-6','25579-4','25580-2','25581-0','25582-8','27408-4','27421-7','27839-0','35195-7','38249-9','38421-4','38422-2',
+													 '38423-0','38424-8','38425-5','38426-3','42180-0','47583-0','47584-8','47585-5','47586-3','47587-1','47588-9','47589-7','47590-5','47591-3','47592-1','47593-9','47594-7','47595-4',
+													 '47832-1','47833-9','47834-7','50461-3','50462-1','50463-9','50464-7','50465-4','50466-2','50467-0','50468-8','55918-7','55919-5','56516-8','56582-0','56583-8','56584-6','57376-6',
+													 '57645-4','57646-2','57647-0','57648-8','57649-6','57650-4','57651-2','57894-8','58494-6','58495-3','58496-1','58497-9','58498-7','58499-5','58500-0','58501-8','58502-6','58503-4',
+													 '58504-2','58505-9','58506-7','58507-5','58508-3','58509-1','58510-9','58511-7','58512-5','58513-3','58514-1','58515-8','58516-6','58517-4','58518-2','58519-0','58520-8','58521-6',
+													 '58522-4','58686-7','58816-0','58841-8','58896-2','77610-4','77611-2','77612-0','77651-8','77652-6',
+							'53061-8',
+							'49779-2','33043-1','33903-6','2514-8','50557-8','5797-6','2514-8','33903-6',
+							'45225-0','45171-6','5265-4','63571-4','56687-7','8086-1','31547-3','34652-8','13927-9','33563-8','31209-0','56718-0','81155-4','32636-3','70253-0','70252-2',
+							'42501-7','13926-1','56540-8','58451-6','81725-4','72523-4','30347-9','83004-2','82660-2',
+							'31209-0','56718-0','81155-4','32636-3','70253-0','70252-2',
+							'76651-9') 
+		or b.RAW_LAB_NAME in ('A1C','LDL','CREATININE','HGB')
+		)
+		and convert(numeric(18,6),(b.LAB_ORDER_DATE - d.BIRTH_DATE))/365.25 between @LowerAge and @UpperAge 
+		and b.LAB_ORDER_DATE between @LowerTimeFrame and @UpperTimeFrame;
+---------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------
+/* Save #NextD_LABS_FINAL as csv file. 
+Use "|" symbol as field terminator and 
+"ENDALONAEND" as row terminator. */ 
+---------------------------------------------------------------------------------------------------------------
