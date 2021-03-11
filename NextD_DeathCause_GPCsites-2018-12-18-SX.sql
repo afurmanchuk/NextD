9ba4337349bcf56617c0219edee7746c0105897b
afurmanchuk <furmanchuk@icnanotox.org>
2021-03-11 09:21:53 -0600

    Update NextD_DeathCause_GPCsites-2018-12-18-SX.sql

--- NextD_DeathCause_GPCsites-2018-12-18-SX.sql
+++ NextD_DeathCause_GPCsites-2018-12-18-SX.sql
@@ -1,24 +1,25 @@
-/******************************************************************************************************************/
-/* NextD Clinical Variable Extractions                                                                            */
-/* - require: 1. FinalStatTable1_local: the local version where dates neither shifted nor masked                  */
-/*                                                                                                                */
-/* - We assume PCORNET_CDM is set appropriate for your site; for example, define PCORNET_CDM = PCORNET_CDM_C5R2   */
-/******************************************************************************************************************/
-
-
-/**************************************************************************/
-/***********************Table 9 -- Death_Cause*****************************/
-/**************************************************************************/
-create table NEXTD_DEATH_CAUSE as
-select fst.PATID,'|' as Pipe1
-      ,dc.DEATH_CAUSE,'|' as Pipe2
-      ,dc.DEATH_CAUSE_CODE,'|' as Pipe3
-      ,dc.DEATH_CAUSE_TYPE,'|' as Pipe4
-      ,dc.DEATH_CAUSE_SOURCE,'|' as Pipe5
-      ,dc.DEATH_CAUSE_CONFIDENCE, 'ENDALONAEND' as ENDOFLINE
-from FinalStatTable1_local fst
-left join "&&PCORNET_CDM".DEATH_CAUSE dc 
-on fst.PATID = dc.PATID
-; 
-
-
+---------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------
+-----                             Part 11: DEATH_CAUSE for the study sample                               -----  
+--------------------------------------------------------------------------------------------------------------- 
+---------------------------------------------------------------------------------------------------------------
+/* Tables for this eaxtraction: 
+1. Table 1 (named here #FinalTable1) with Next-D study sample IDs. See separate SQL code for producing this table.
+2. DEATH_CAUSE table from PCORNET. */
+---------------------------------------------------------------------------------------------------------------
+use /*provide here the name of PCORI database here: */PCORI_SAS;
+select c.PATID,
+		b.DEATH_CAUSE, 
+		b.DEATH_CAUSE_CODE,
+		b.DEATH_CAUSE_TYPE ,
+		b.DEATH_CAUSE_SOURCE ,
+		b.DEATH_CAUSE_CONFIDENCE 
+into #NextD_DEATH_CAUSE_FINAL
+from /* provide name of table 1 here: */ #FinalTable1 c 
+left join [dbo].[DEATH_CAUSE] b on c.PATID=b.PATID;
+---------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------
+/* Save #NextD_DEATH_CAUSE_FINAL as csv file. 
+Use "|" symbol as field terminator and 
+"ENDALONAEND" as row terminator. */ 
+---------------------------------------------------------------------------------------------------------------
