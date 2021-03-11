e1c30c4f081db3292c26df833958ba03ec77d867
afurmanchuk <furmanchuk@icnanotox.org>
2021-03-11 09:23:02 -0600

    Update NextD_Diagnoses_GPCsites-2019-11-25-SX.sql

--- NextD_Diagnoses_GPCsites-2019-11-25-SX.sql
+++ NextD_Diagnoses_GPCsites-2019-11-25-SX.sql
@@ -1,40 +1,45 @@
-/******************************************************************************************************************/
-/* NextD Clinical Variable Extractions - Diagnosis Table                                                          */
-/* exclude pregancy                                                                                               */
-/******************************************************************************************************************/
-
-/* Tables required in this code: 
-- 1. NEXTD_ENCOUNTER: output table of NextD_Encounter_GPCsites-2019-11-25-SX.sql 
-- 2. NextD_preg_masked_encounters: an intermediate table with non-pregnacy encounters
-- 3. &&PCORNET_CDM.DIAGNOSIS                                  
-*/
-
-/*global parameters:
- &&PCORNET_CDM: name of CDM schema (>v5.0)
- "KUMC specific" fields: may need to be adjusted with local EMR values
-*/
-
-create table NEXTD_DIAGNOSIS as
-select dx.PATID,'|' as Pipe1
-      ,dx.ENCOUNTERID,'|' as Pipe2
-      ,dx.DIAGNOSISID,'|' as Pipe3
-      ,dx.DX,'|' as Pipe4
-      ,dx.PDX,'|' as Pipe5
-      ,dx.DX_TYPE,'|' as Pipe6
-      ,dx.DX_SOURCE,'|' as Pipe7
-      ,dx.DX_ORIGIN,'|' as Pipe8
-      ,enc.ENC_TYPE,'|' as Pipe9
-      ,enc.ADMIT_YEAR,'|' as Pipe10
-      ,enc.ADMIT_MONTH,'|' as Pipe11
-      ,enc.ADMIT_Days_from_FirstEnc
-      ,'ENDALONAEND' as ENDOFLINE
-from "&&PCORNET_CDM".DIAGNOSIS dx
-join NEXTD_ENCOUNTER enc on enc.ENCOUNTERID = dx.ENCOUNTERID
-where exists (select 1 from NextD_preg_masked_encounters exclud_pregn
-              where dx.ENCOUNTERID = exclud_pregn.ENCOUNTERID)
-;
-
 ---------------------------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------------------
------ 1. Download table NEXTD_DIAGNOSIS as .csv file for final delivery                                  ------
+-----                             Part 4: Diagnoses for the study sample                                  -----  
+--------------------------------------------------------------------------------------------------------------- 
+---------------------------------------------------------------------------------------------------------------
+/* Tables for this eaxtraction: 
+1. Table 1 (named here #FinalTable1) with Next-D study sample IDs. See separate SQL code for producing this table.
+2. DIAGNOSIS and DEMOGRAPHIC table from PCORNET. */
+---------------------------------------------------------------------------------------------------------------
+use /*provide here the name of PCORI database here: */PCORI_SAS;
+---------------------------------------------------------------------------------------------------------------
+-----                            Declare study time frame variables:                                      -----
+DECLARE @studyTimeRestriction int;declare @UpperTimeFrame int; declare @LowerTimeFrame int;
+-----                             Specify time frame and age limits                                       -----
+--Set extraction time frame below. If time frames not set, the code will use the whole time frame available from the database
+set @LowerTimeFrame=18263;--'2010-01-01';
+set @UpperTimeFrame=22280;--'2020-12-31';
+--set age restrictions:
+declare @UpperAge int; declare @LowerAge int;set @UpperAge=89; set @LowerAge=18;
+---------------------------------------------------------------------------------------------------------------
+select c.PATID,
+		b.ENCOUNTERID,
+		b.DIAGNOSISID,
+		b.DX,
+		b.PDX,
+		b.DX_POA,
+		b.DX_TYPE,
+		b.DX_SOURCE,
+		b.DX_ORIGIN,
+		b.ENC_TYPE,
+		year(b.ADMIT_DATE) as ADMIT_DATE_YEAR,
+		month(b.ADMIT_DATE) as ADMIT_DATE_MONTH,
+		b.ADMIT_DATE - c.FirstVisit as DAYS_from_FirstEncounter_Date
+into #NextD_DIAGNOSIS_FINAL 
+from /* provide name of table 1 here: */ #FinalTable1 c 
+join  dbo.DIAGNOSIS b on c.PATID=b.PATID 
+join dbo.DEMOGRAPHIC d on c.PATID=d.PATID
+where convert(numeric(18,6),(b.ADMIT_DATE-d.BIRTH_DATE))/365.25 between @LowerAge and @UpperAge 
+	and b.ADMIT_DATE between @LowerTimeFrame and @UpperTimeFrame;
+---------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------
+/* Save #NextD_DIAGNOSIS_FINAL as csv file. 
+Use "|" symbol as field terminator and 
+"ENDALONAEND" as row terminator. */ 
 ---------------------------------------------------------------------------------------------------------------
