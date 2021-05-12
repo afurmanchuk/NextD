9a50c978edd8242905cd901832c4876adc7cbfc1
afurmanchuk <furmanchuk@icnanotox.org>
2021-05-12 09:09:53 -0500

    Update MS_NextD_ExtractionCode_PROCEDURES.sql

--- MS_NextD_ExtractionCode_PROCEDURES.sql
+++ MS_NextD_ExtractionCode_PROCEDURES.sql
@@ -18,22 +18,22 @@ set @UpperTimeFrame=22280;--'2020-12-31';
 --set age restrictions:
 declare @UpperAge int; declare @LowerAge int;set @UpperAge=89; set @LowerAge=18;
 ---------------------------------------------------------------------------------------------------------------
-select c.PATID,
-	b.ENCOUNTERID,
-	b.PROCEDURESID,
-	b.ENC_TYPE,
-	year(dateadd(dd,b.ADMIT_DATE,'1960-01-01')) as ADMIT_DATE_YEAR,
-	month(dateadd(dd,b.ADMIT_DATE,'1960-01-01')) as ADMIT_DATE_MONTH,
-	b.ADMIT_DATE - c.FirstVisit as DAYS_from_FirstEncounter_Date1,
-	b.PROVIDERID,
-	year(dateadd(dd,b.PX_DATE,'1960-01-01')) as PX_DATE_YEAR,
-	month(dateadd(dd,b.PX_DATE,'1960-01-01')) as PX_DATE_MONTH,
-	b.PX_DATE - c.FirstVisit as DAYS_from_FirstEncounter_Date2,
-	b.PX,
-	b.PPX,
-	b.PX_TYPE ,
-	b.PX_SOURCE
-	into #NextD_PROCEDURES_FINAL
+select c.PATID, '|' as Pipe1,
+	b.ENCOUNTERID, '|' as Pipe2,
+	b.PROCEDURESID, '|' as Pipe3,
+	b.ENC_TYPE, '|' as Pipe4,
+	year(dateadd(dd,b.ADMIT_DATE,'1960-01-01')) as ADMIT_DATE_YEAR, '|' as Pipe5,
+	month(dateadd(dd,b.ADMIT_DATE,'1960-01-01')) as ADMIT_DATE_MONTH, '|' as Pipe6,
+	b.ADMIT_DATE - c.FirstVisit as DAYS_from_FirstEncounter_Date1, '|' as Pipe7,
+	b.PROVIDERID, '|' as Pipe8,
+	year(dateadd(dd,b.PX_DATE,'1960-01-01')) as PX_DATE_YEAR, '|' as Pipe9,
+	month(dateadd(dd,b.PX_DATE,'1960-01-01')) as PX_DATE_MONTH, '|' as Pipe10,
+	b.PX_DATE - c.FirstVisit as DAYS_from_FirstEncounter_Date2, '|' as Pipe11,
+	b.PX, '|' as Pipe12,
+	b.PPX, '|' as Pipe13,
+	b.PX_TYPE , '|' as Pipe14,
+	b.PX_SOURCE,'ENDALONAEND' as lineEND
+into #NextD_PROCEDURES_FINAL
 from /* provide name of table 1 here: */ #FinalTable1 c 
 join /* provide name of PCORNET table PROCEDURES here: */ [dbo].[PROCEDURES] b on c.PATID=b.PATID 
 join /* provide name of PCORNET table DEMOGRAPPHIC here: */ [dbo].DEMOGRAPHIC d on c.PATID=d.PATID
