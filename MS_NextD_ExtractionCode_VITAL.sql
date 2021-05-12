4a73442172cd0cd9baabfd29c0eb8d53f00222bb
afurmanchuk <furmanchuk@icnanotox.org>
2021-05-12 09:15:04 -0500

    Update MS_NextD_ExtractionCode_VITAL.sql

--- MS_NextD_ExtractionCode_VITAL.sql
+++ MS_NextD_ExtractionCode_VITAL.sql
@@ -15,16 +15,18 @@ set @UpperTimeFrame=22280;--'2020-12-31';
 --set age restrictions:
 declare @UpperAge int; declare @LowerAge int;set @UpperAge=89; set @LowerAge=18;
 ---------------------------------------------------------------------------------------------------------------
-select c.[PATID],b.[VITALID],b.[ENCOUNTERID],		
-			b.VITAL_SOURCE,
-			b.HT,
-			b.WT,
-			b.DIASTOLIC,
-			b.SYSTOLIC,
-			b.SMOKING,
-			year(dateadd(dd,b.MEASURE_DATE,'1960-01-01')) as MEASURE_DATE_YEAR,
-			month(dateadd(dd,b.MEASURE_DATE,'1960-01-01')) as MEASURE_DATE_MONTH,
-			b.MEASURE_DATE-c.FirstVisit as DAYS_from_FirstEncounter_Date
+select c.[PATID], '|' as Pipe1,
+			b.[VITALID], '|' as Pipe2,
+			b.[ENCOUNTERID], '|' as Pipe3,	
+			b.VITAL_SOURCE, '|' as Pipe4,
+			b.HT, '|' as Pipe5,
+			b.WT, '|' as Pipe6,
+			b.DIASTOLIC, '|' as Pipe7,
+			b.SYSTOLIC, '|' as Pipe8,
+			b.SMOKING, '|' as Pipe9,
+			year(dateadd(dd,b.MEASURE_DATE,'1960-01-01')) as MEASURE_DATE_YEAR, '|' as Pipe10,
+			month(dateadd(dd,b.MEASURE_DATE,'1960-01-01')) as MEASURE_DATE_MONTH, '|' as Pipe11,
+			b.MEASURE_DATE-c.FirstVisit as DAYS_from_FirstEncounter_Date,'ENDALONAEND' as lineEND
 into #NextD_VITAL_FINAL
 from /* provide name of table 1 here: */ #FinalTable1 c 
 join /* provide name of PCORNET table VITAL here: */  [dbo].[VITAL] b on c.PATID=b.PATID
