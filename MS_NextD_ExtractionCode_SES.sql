0f73e8dd02bb59682f010069ed98c9e84b927428
afurmanchuk <furmanchuk@icnanotox.org>
2021-05-12 09:13:18 -0500

    Update MS_NextD_ExtractionCode_SES.sql

--- MS_NextD_ExtractionCode_SES.sql
+++ MS_NextD_ExtractionCode_SES.sql
@@ -11,13 +11,7 @@
 ---------------------------------------------------------------------------------------------------------------
 --------------------------------------------------------------------------------------------------------------
 /*use code below if you are member of GPC consortium:*/
-select c.PATID,b.GTRACT_ACS 
-into #NextD_SES
-from /* provide name of table 1 here: */ #FinalTable1 c 
-left join /* provide name of non-PCORNET table with SES data here: */ #SES b on c.PATID=b.PATID;
-
-/*use code below if you are member of CAPRICORN consortium:*/
-select c.PATID,b.*/*here will be list of SES variables for each patient */
+select c.PATID, '|' as Pipe1,b.GTRACT_ACS ,'ENDALONAEND' as lineEND
 into #NextD_SES
 from /* provide name of table 1 here: */ #FinalTable1 c 
 left join /* provide name of non-PCORNET table with SES data here: */ #SES b on c.PATID=b.PATID;
