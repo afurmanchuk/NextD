d4b2755e7b93d80b001996187c16d39b713f3db8
afurmanchuk <furmanchuk@icnanotox.org>
2021-05-20 08:27:50 -0500

    Update MS_NextD_ExtractionCode_DIAGNOSIS.sql

--- MS_NextD_ExtractionCode_DIAGNOSIS.sql
+++ MS_NextD_ExtractionCode_DIAGNOSIS.sql
@@ -13,8 +13,8 @@ use /*provide here the name of PCORI database here: */PCORI_SAS;
 DECLARE @studyTimeRestriction int;declare @UpperTimeFrame int; declare @LowerTimeFrame int;
 -----                             Specify time frame and age limits                                       -----
 --Set extraction time frame below. If time frames not set, the code will use the whole time frame available from the database
-set @LowerTimeFrame=18263;--'2010-01-01';
-set @UpperTimeFrame=22280;--'2020-12-31';
+set @LowerTimeFrame='2010-01-01';
+set @UpperTimeFrame='2020-12-31';
 --set age restrictions:
 declare @UpperAge int; declare @LowerAge int;set @UpperAge=89; set @LowerAge=18;
 ---------------------------------------------------------------------------------------------------------------
@@ -28,14 +28,14 @@ select c.PATID, '|' as Pipe1,
 		b.DX_SOURCE, '|' as Pipe8,
 		b.DX_ORIGIN, '|' as Pipe9,
 		b.ENC_TYPE, '|' as Pipe10,
-		year(dateadd(dd,b.ADMIT_DATE,'1960-01-01')) as ADMIT_DATE_DATE_YEAR, '|' as Pipe11,
-		month(dateadd(dd,b.ADMIT_DATE,'1960-01-01')) as ADMIT_DATE_DATE_MONTH, '|' as Pipe12,
-		b.ADMIT_DATE - c.FirstVisit as DAYS_from_FirstEncounter_Date,'ENDALONAEND' as lineEND
+		year(b.ADMIT_DATE) as ADMIT_DATE_DATE_YEAR, '|' as Pipe11,
+		month(b.ADMIT_DATE) as ADMIT_DATE_DATE_MONTH, '|' as Pipe12,
+		datediff(d,c.FirstVisit,b.ADMIT_DATE) as DAYS_from_FirstEncounter_Date,'ENDALONAEND' as lineEND
 into #NextD_DIAGNOSIS_FINAL 
 from /* provide name of table 1 here: */ #FinalTable1 c 
 join  dbo.DIAGNOSIS b on c.PATID=b.PATID 
 join dbo.DEMOGRAPHIC d on c.PATID=d.PATID
-where convert(numeric(18,6),(b.ADMIT_DATE-d.BIRTH_DATE))/365.25 between @LowerAge and @UpperAge 
+where datediff(yy,d.BIRTH_DATE,b.ADMIT_DATE) between @LowerAge and @UpperAge 
 	and b.ADMIT_DATE between @LowerTimeFrame and @UpperTimeFrame;
 ---------------------------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------------------
