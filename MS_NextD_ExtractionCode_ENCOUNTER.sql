f8144445f9ffdb38fe10d647a5777a11d64c3608
afurmanchuk <furmanchuk@icnanotox.org>
2021-05-20 08:32:02 -0500

    Update MS_NextD_ExtractionCode_ENCOUNTER.sql

--- MS_NextD_ExtractionCode_ENCOUNTER.sql
+++ MS_NextD_ExtractionCode_ENCOUNTER.sql
@@ -12,8 +12,8 @@ use /*provide here the name of PCORI database here: */PCORI_SAS;
 DECLARE @studyTimeRestriction int;declare @UpperTimeFrame int; declare @LowerTimeFrame int;
 -----                             Specify time frame and age limits                                       -----
 --Set extraction time frame below. If time frames not set, the code will use the whole time frame available from the database
-set @LowerTimeFrame=18263;--'2010-01-01';
-set @UpperTimeFrame=22280;--'2020-12-31';
+set @LowerTimeFrame='2010-01-01';
+set @UpperTimeFrame='2020-12-31';
 --set age restrictions:
 declare @UpperAge int; declare @LowerAge int;set @UpperAge=89; set @LowerAge=18;
 ---------------------------------------------------------------------------------------------------------------
@@ -30,12 +30,12 @@ declare @UpperAge int; declare @LowerAge int;set @UpperAge=89; set @LowerAge=18;
 select c.PATID,'|' as Pipe,1
 		a.ENCOUNTERID,'|' as Pipe2,
 		a.PROVIDERID,'|' as Pipe3,
-		year(dateadd(dd,a.ADMIT_DATE,'1960-01-01')) as ADMIT_DATE_YEAR,'|' as Pipe4,
-		month(dateadd(dd,a.ADMIT_DATE,'1960-01-01')) as ADMIT_DATE_MONTH,'|' as Pipe5,
-		a.ADMIT_DATE - c.[FirstVisit] as DAYS_from_FirstEncounter_Date1,'|' as Pipe6,
-		year(dateadd(dd,a.DISCHARGE_DATE,'1960-01-01')) as DISCHARGE_DATE_YEAR,'|' as Pipe7,
-		month(dateadd(dd,a.DISCHARGE_DATE,'1960-01-01')) as DISCHARGE_DATE_MONTH,'|' as Pipe8,
-		a.DISCHARGE_DATE - c.[FirstVisit] as DAYS_from_FirstEncounter_Date2,'|' as Pipe9,
+		year(a.ADMIT_DATE) as ADMIT_DATE_YEAR,'|' as Pipe4,
+		month(a.ADMIT_DATE) as ADMIT_DATE_MONTH,'|' as Pipe5,
+		datediff(dd,c.[FirstVisit],a.ADMIT_DATE) as DAYS_from_FirstEncounter_Date1,'|' as Pipe6,
+		year(a.DISCHARGE_DATE) as DISCHARGE_DATE_YEAR,'|' as Pipe7,
+		month(a.DISCHARGE_DATE) as DISCHARGE_DATE_MONTH,'|' as Pipe8,
+		datediff(dd,c.[FirstVisit],a.DISCHARGE_DATE) as DAYS_from_FirstEncounter_Date2,'|' as Pipe9,
 		a.ENC_TYPE,'|' as Pipe10,
 		a.FACILITYID,'|' as Pipe11,
 		a.FACILITY_TYPE,'|' as Pipe12,
@@ -48,6 +48,6 @@ into #NextD_ENCOUNTER_FINAL
 from /* provide name of table 1 here: */ #FinalTable1 c 
 join [dbo].[ENCOUNTER] a on c.PATID=a.PATID
 join dbo.DEMOGRAPHIC d on c.PATID=d.PATID		
-where convert(numeric(18,6),(a.ADMIT_DATE-d.BIRTH_DATE))/365.25 between @LowerAge and @UpperAge 
+where datediff(yy,d.BIRTH_DATE,a.ADMIT_DATE) between @LowerAge and @UpperAge 
 and a.ADMIT_DATE between @LowerTimeFrame and @UpperTimeFrame; 
 --------------------------------------------------------------------------------------------------------------- 
