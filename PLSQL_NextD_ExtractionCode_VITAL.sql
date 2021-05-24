74d8876d75c2e1214c815d1ea58922e2b70c3497
afurmanchuk <furmanchuk@icnanotox.org>
2021-05-24 07:18:01 -0500

    Update PLSQL_NextD_ExtractionCode_VITAL.sql

--- PLSQL_NextD_ExtractionCode_VITAL.sql
+++ PLSQL_NextD_ExtractionCode_VITAL.sql
@@ -6,7 +6,9 @@
 2. VITAL and DEMOGRAPHIC table from PCORNET.*/
 ---------------------------------------------------------------------------------------------------------------
 ----  Declare study time frame variables:
------                             Specify age limits                                       -----
+--Set your time frame below between '2010-01-01' and '2020-12-31'. If time frames not set, the code will use the whole time frame available from the database;
+define LowerTimeFrame= TO_DATE('2010-01-01' , 'YYYY-MM-DD') 
+define UpperTimeFrame= TO_DATE('2020-12-31', 'YYYY-MM-DD')
 --set age restrictions:
 define UpperAge=89 
 define LowerAge=18
@@ -17,23 +19,21 @@ drop table NextD_VITAL_FINAL;
 whenever sqlerror exit;
 
 create table NextD_VITAL_FINAL as 
-    select c.PATID, '|' as Pipe1,
-    			b.VITALID, '|' as Pipe2,
-			b.ENCOUNTERID, '|' as Pipe3,		
-			b.VITAL_SOURCE, '|' as Pipe4,
-			b.HT, '|' as Pipe5,
-			b.WT, '|' as Pipe6,
-			b.DIASTOLIC, '|' as Pipe7,
-			b.SYSTOLIC, '|' as Pipe8,
-			b.SMOKING, '|' as Pipe9,
-			EXTRACT(year FROM b.MEASURE_DATE) as MEASURE_DATE_YEAR, '|' as Pipe10,
-			EXTRACT(month FROM b.MEASURE_DATE) as MEASURE_DATE_MONTH, '|' as Pipe11,
-			b.MEASURE_DATE-c.FirstVisit as DAYS_from_FirstEncounter_Date,'ENDALONAEND' as lineEND
+    select c.PATID,b.VITALID,b.ENCOUNTERID,		
+			b.VITAL_SOURCE,
+			b.HT,
+			b.WT,
+			b.DIASTOLIC,
+			b.SYSTOLIC,
+			b.SMOKING,
+			EXTRACT(year FROM b.MEASURE_DATE) as MEASURE_DATE_YEAR,
+			EXTRACT(month FROM b.MEASURE_DATE) as MEASURE_DATE_MONTH,
+			b.MEASURE_DATE-c.FirstVisit as DAYS_from_FirstEncounter_Date
 from FinalTable1 c 
 join "&&PCORNET_CDM".VITAL b on c.PATID=b.PATID -- provide here the name of PCORI databas
 join "&&PCORNET_CDM".DEMOGRAPHIC d on c.PATID=d.PATID  -- provide here the name of PCORI databas
 where cast((b.MEASURE_DATE-d.BIRTH_DATE) as numeric(18,6))/365.25 between &LowerAge and &UpperAge 
-	and b.MEASURE_DATE between TO_DATE('2010-01-01', 'YYYY-MM-DD') and TO_DATE('2020-12-31', 'YYYY-MM-DD') ; --Set extraction time frame
+	and b.MEASURE_DATE between &LowerTimeFrame and &UpperTimeFrame ; --Set extraction time frame
 ---------------------------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------------------
 /* Save #NextD_VITAL_FINAL as csv file. 
